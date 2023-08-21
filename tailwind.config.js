4d6e85cbb786bff537dea252823cd6d5aba76635
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-08-21 12:01:14 +0300

    Initial Project setup - React & Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    `./index.html`,
+    `./src/**/*.{js,jsx}`
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// This has to be reviewed if its properly configured later
