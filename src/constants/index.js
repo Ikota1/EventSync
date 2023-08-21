15bca8c540c0700bce6da257b55e2f0acb59c6f0
Daniil <dnazarov870@gmail.com>
2023-08-21 12:41:24 +0300

    Adding Custom Styles and Setting of Tailwind

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "events",
+    title: "Events",
+  },
+  {
+    id: "calendar",
+    title: "Calendar",
+  },
+  {
+    id: "social&friends",
+    title: "Social & Friends",
+  },
+];
