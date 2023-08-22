286095f358dcc1a712bc609c1ac4b2edf99a0486
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-08-23 01:13:19 +0300

    Add AuthContextProvider to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import AppRouter from "./components/AppRouter";
 import { BrowserRouter } from "react-router-dom";
 import LandingPage from "./views/LandingPage/LandingPage";
+import { AuthContextProvider } from "./context/UserContext"; 
 
 const App = () => (
+  <AuthContextProvider>
   <BrowserRouter>
     <div className="bg-primary w-full overflow-hidden">
       {<LandingPage /> && <AppRouter />}
     </div>
   </BrowserRouter>
+  </AuthContextProvider>
 )
 
 
