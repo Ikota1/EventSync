95dab64df16c9249ccfbf6f362e87359ff7c9f12
Daniil <dnazarov870@gmail.com>
2023-08-22 16:39:11 +0300

    Updated file for images

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,18 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 import arrowUp from './arrow-up.svg';
 import logo from './logo.svg';
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import quotes from "./quotes.svg";
 
 export {
   menu,
   close,
   arrowUp,
   logo,
+  people01,
+  people02,
+  people03,
+  quotes,
 };
