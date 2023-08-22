83a35b023907c445534672f555093afb636f3a66
Daniil <dnazarov870@gmail.com>
2023-08-22 16:35:51 +0300

    Update Landing Page

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -1,6 +1,9 @@
 import styles from '../../style';
 import Hero from '../../components/Hero/Hero';
 import Navbar from '../../components/Navbar/Navbar';
+import Stats from '../../components/Stats/Stats';
+import Testimonials from '../../components/Testimonials/Testimonials';
+import Footer from '../../components/Footer/Footer';
 
 const LandingPage = () => (
   <>
@@ -13,6 +16,14 @@ const LandingPage = () => (
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Testimonials />
+        <Footer />
       </div>
     </div>
   </>
