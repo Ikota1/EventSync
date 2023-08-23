14b522c63fc9ba6b50ea913a435b31b1f0738074
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-08-23 22:45:39 +0300

    Add AccordionConstants.js

---
+++ src/constants/UIconstants/accordionConstants.js
@@ -0,0 +1,33 @@
+export const accordionConstants = [
+  {
+    id: "accordion-1",
+    title: "Getting Started",
+    content:
+      "Welcome to EventSync the premier web application designed to revolutionize the way you discover, create and connect through events! Whether you`re a social butterfly, an event organizer, or someone looking to explore exciting activities, EventSync is your all-in-one solution. Get ready to immerse yourself in a world of endless possibilities, where every event is just a click away. Explore a World of Events, Create and Customize, Connect with Like-Minded Individuals, Stay Informed, Seamless Planning and Participation and Share and Spread the Word. Join the EventSync Revolution Today!",
+  },
+  {
+    id: "accordion-2",
+    title:
+      "What are the difference between Public user, User and Premium user?",
+    content:
+      "The Public user has a restricted access, and is allow to few features of the sites such as: Landing Page, News, FAQ and Sign up. After successful registration the User has access to the main interface, where event can be browsed, searched and bought. Furthermore the User has access to schedule calendar where he/she can track events. The Premium user on top of the rights of the User, can create events and sent gifts to other users ",
+  },
+  {
+    id:"accordion-3",
+    title:"Can Users sent friend requests?",
+    content:
+    "Yes, after registration users can sent friend request to one another. However if an user want to sent gift to another user, a Premium package is needed.",
+  },
+  {
+    id:"accordion-4",
+    title:"Can I track upcoming events?",
+    content:
+    "Yes! Users can track upcoming events in the calendar. It is a feature which allow better visual representation of all the events an user is interest in."
+  },
+  {
+    id:"accordion-5",
+    title:"Why I cannot invite friends to an event?",
+    content:
+    "To be able to sent invitation to a friend, you either should be the creator / owner of the event, or to be given such right by the event owner. However, if you are User you do not have the rights to create an event. To do so you should upgrade to Premium user."
+  }
+];
