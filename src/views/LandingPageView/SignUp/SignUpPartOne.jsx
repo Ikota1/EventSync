f7acddde84de96ad19d7178ec90b556a2442208d
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-08-24 00:23:51 +0300

    Remove 'remember me' input

--- src/views/LandingPageView/SignUp/SignUpPartOne.jsx
+++ src/views/LandingPageView/SignUp/SignUpPartOne.jsx
@@ -90,23 +90,23 @@ const SignUpPartOne = ({ stageOneFormData }) => {
               </div>
               <div className="flex justify-between">
 
-
+               {/* we can change the button with a back icon or something */}
                 <NavLink to='/'>
-                  <button
-                    type='submit'
-                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                  >
-                    Back to
-                  </button>
-
-
-                </NavLink>
+                <button
+                  type='submit'
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Back
+                </button>
+                  
+  
+                  </NavLink>
 
                 <button
                   type='submit'
                   className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
-                  Cont
+                  Continue
                 </button>
 
               </div>
