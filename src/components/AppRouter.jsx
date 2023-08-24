c502778f665dd091f8a20ec1582508e707b49c73
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-08-24 03:28:13 +0300

    Add AuthRoute for applicationView

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,5 +1,4 @@
-import { Routes, Route } from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "../views/LandingPageView/LandingPage/LandingPage";
 import Login from "../views/LandingPageView/Login/Login";
 import SignUpPartTwo from "../views/LandingPageView/SignUp/SignUpPartTwo";
@@ -7,6 +6,14 @@ import SignUp from "../views/LandingPageView/SignUp/SignUp";
 import News from "../views/LandingPageView/News/News";
 import FAQ from "../views/LandingPageView/FAQ/FAQ";
 import ApplicationView from "../views/WebApplicationView/ApplicationView";
+import { auth } from "../firebase/firebase-config";
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+
+const AuthenticatedRoute = ({ element }) => {
+  const [user] = useAuthState(auth);
+  return user ? element : <Navigate to="/login" />;
+};
 
 const AppRouter = () => (
   <Routes>
@@ -17,9 +24,7 @@ const AppRouter = () => (
     <Route path="/login" element={<Login />} />
     <Route path="/signup" element={<SignUp />} />
     <Route path="/signupparttwo" element={<SignUpPartTwo />} />
-
-    {/* For logged in Application view */}
-    <Route path="/application" element={<ApplicationView />} />
+    <Route exact path="/application" element={<AuthenticatedRoute element={<ApplicationView />} />} />
   </Routes>
 )
 
