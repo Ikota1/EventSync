9339ec3616f2713a956729babe7814e7f5f711da
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-08-27 19:01:06 +0300

    Add UserProfile.jsx

--- src/views/WebApplicationView/Settings/Settings.jsx
+++ src/views/WebApplicationView/Settings/Settings.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { UserProfile } from '../UserProfile/UserProfile'
 
 const Settings = () => {
   return (
+    <>
     <div className='text-white'>Settings</div>
+    <UserProfile />
+    </>
   )
 }
 
