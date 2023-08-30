0ac6ca94ec174947069b807278a2786f47602b92
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-08-30 19:58:53 +0300

    Update styling of application/events View

--- src/views/WebApplicationView/Events.jsx/Events.jsx
+++ src/views/WebApplicationView/Events.jsx/Events.jsx
@@ -3,37 +3,31 @@ import CreateEventButton from "../../../components/CreateEventButton/CreateEvent
 
 const Events = () => {
   const handleEventCreated = () => {
-    // TBD mb
+    // TBD 
   };
 
   return (
     <div className="bg-event-gradient min-h-screen">
       <div className="container mx-auto pt-16 px-6">
-        <h1 className="text-3xl font-semibold text-white mb-6">Events</h1>
-        <CreateEventButton onEventCreated={handleEventCreated} />
+        <div className='flex justify-end'>
+          <CreateEventButton onEventCreated={handleEventCreated} />
 
-        <nav className="mt-6">
-          <ul className="flex space-x-6">
-            <li>
-              <Link to="public-events" className="text-white hover:text-blue-300">
-                Public Events
-              </Link>
-            </li>
-            <li>
-              <Link to="my-events" className="text-white hover:text-blue-300">
-                My Events
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="flex items-center mt-6">
-          <input
-            type="text"
-            placeholder="Search events..."
-            className="bg-white rounded-l-md p-2 focus:outline-none w-64"
-          />
-          <button className="bg-blue-500 text-white rounded-r-md p-2 hover:bg-blue-600">
-          </button>
+        </div>
+
+        <div className='flex justify-center pb-16'>
+          <nav className="mt-6">
+            <ul className="flex space-x-6">
+              <li><Link to="public-events" className="text-blue-500 hover:text-blue-300"> Public Events</Link></li>
+              <li><Link to="my-events" className="text-blue-500 hover:text-blue-300"> My Events</Link></li>
+             </ul>
+          </nav>
+          <div className="flex items-center mt-4 ml-6">
+            <input
+              type="text"
+              placeholder="Search events..."
+              className="bg-white rounded-l-md p-2 focus:outline-none w-64"/>
+            <button className="bg-blue-500 text-white rounded-r-md p-2 hover:bg-blue-600"></button>
+          </div>
         </div>
         <Outlet />
       </div>
