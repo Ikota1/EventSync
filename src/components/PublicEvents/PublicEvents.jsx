20d2e81e00e46f145e06f24acca5f9e6673bfd7f
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-08-31 05:08:07 +0300

    Add live-search for Public Events in the app

--- src/components/PublicEvents/PublicEvents.jsx
+++ src/components/PublicEvents/PublicEvents.jsx
@@ -4,21 +4,25 @@ import { getPublicEvents } from '../../services/events.service';
 const PublicEvents = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [publicEvents, setPublicEvents] = useState([]);
-
-  const eventsPerPage = 6; 
+  const [searchItem, setSearchItem] = useState('');
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const eventsPerPage = 6;
 
   useEffect(() => {
     const fetchPublicEvents = async () => {
-      const startIndex = (currentPage - 1) * eventsPerPage;
-      const endIndex = startIndex + eventsPerPage;
-      const publicEventsData = await getPublicEvents(); 
-
-      const paginatedEvents = publicEventsData.slice(startIndex, endIndex);
-      setPublicEvents(paginatedEvents);
+      const publicEventsData = await getPublicEvents();
+      setPublicEvents(publicEventsData);
     };
 
     fetchPublicEvents();
-  }, [currentPage, publicEvents]);
+  }, [searchItem, publicEvents]);
+
+  useEffect(() => {
+    const filteredItems = publicEvents.filter((event) => event.title.toLowerCase().includes(searchItem.toLowerCase())
+    );
+
+    setFilteredEvents(filteredItems);
+  }, [searchItem, publicEvents]);
 
   const handleNextPage = () => {
     setCurrentPage(currentPage + 1);
@@ -32,9 +36,17 @@ const PublicEvents = () => {
 
   return (
     <div>
-      <h2>Public Events</h2>
+      <div className="flex justify-center mb-12">
+        <input
+          type="text"
+          placeholder="Search events..."
+          value={searchItem}
+          onChange={(e) => setSearchItem(e.target.value)}
+          className="bg-white rounded-l-md p-2 focus:outline-none w-64"
+        />
+      </div>
       <div className="grid grid-cols-3 gap-2">
-        {publicEvents.map(event => (
+        {filteredEvents.slice((currentPage - 1) * eventsPerPage, currentPage * eventsPerPage).map((event) => (
           <div key={event.id} className="bg-white rounded-lg shadow p-4">
             <h3 className="text-lg font-semibold">Event: {event.title}</h3>
             <p>Description: {event.description}</p>
@@ -47,15 +59,13 @@ const PublicEvents = () => {
 
       {/* Pagination controls */}
       <div className={`fixed bottom-0 right-0 py-2 px-6 shadow`}>
-      <div className="pagination text-blue-500">
-        <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-          Page
-        </button>
-        <span className='mx-3 my-3'>{currentPage}</span>
-        <button onClick={handleNextPage}>
-          Next Page
-        </button>
-      </div>
+        <div className="pagination text-blue-500">
+          <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+            Page
+          </button>
+          <span className="mx-3 my-3">{currentPage}</span>
+          <button onClick={handleNextPage}>Next Page</button>
+        </div>
       </div>
     </div>
   );
