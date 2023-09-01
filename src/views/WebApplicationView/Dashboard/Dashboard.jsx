0fed071820f84470bf88650057346dd26353502b
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-01 23:51:01 +0300

    Update Dashboard.jsx, Add visual information

--- src/views/WebApplicationView/Dashboard/Dashboard.jsx
+++ src/views/WebApplicationView/Dashboard/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import WeatherForecast from '../../../components/Weather/Weather'
 
 const Dashboard = () => {
   return (
+    <>
     <div className='text-white'>Dashboard</div>
+    <WeatherForecast />
+    </>
   )
 }
 
