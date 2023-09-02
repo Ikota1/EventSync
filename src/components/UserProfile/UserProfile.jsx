cab4e6734a95d0234c0f6289ef53719c1bff8bea
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-02 23:14:55 +0300

    Update UserProfile.jsx; refactor code

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,24 +1,22 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/UserContext";
 import { Link } from "react-router-dom";
 import UserEditProfileButton from "../UserEditProfileButton/UserEditProfileButton";
 
 export const UserProfile = () => {
   const { userData } = useContext(AuthContext);
-  console.log(userData);
-
+    
   if (!userData) {
     return <p>Loading...</p>;
   }
 
   const getInitials = (firstName, lastName) => {
-    return `${firstName.charAt(0) || ""}${lastName.charAt(0) || ""
-      }`.toUpperCase();
+    return `${firstName.charAt(0) || ""}${lastName.charAt(0) || ""}`.toUpperCase();
   };
 
   return (
     <div className="p-16">
-      <div className="p-8 bg-white shadow mt-24 rounded-lg">
+      <div className="p-8 bg-white shadow mt-24">
         <div className="grid grid-cols-1 md:grid-cols-3">
           <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
             <div>
@@ -39,13 +37,15 @@ export const UserProfile = () => {
             </div>
           </div>
           <div className="relative">
-            <div className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
-              {userData && userData.photo ? (
-                <img
-                className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl flex items-center justify-center text-indigo-500"
-                  src={userData.photo}
-                  alt={getInitials(userData.firstName, userData.lastName)}
-                />
+            <div className={`w-48 h-48 bg-indigo-100 mx-auto rounded-full border-2 ring-offset-2 ring-4 ${userData && userData.isActive === true ? "ring-green-500" : "ring-red-500"} ring-green-500 shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500`}>
+              {userData && userData?.photo ? (
+                <div className="relative">
+                  <img
+                    className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl flex items-center justify-center text-indigo-500"
+                    src={userData?.photo}
+                    alt={getInitials(userData.firstName, userData.lastName)}
+                  />
+                </div>
               ) : (
                 <span className="font-normal font-poppins text-3xl text-white dark:text-slate-500">
                   {getInitials(userData.firstName, userData.lastName)}
@@ -56,7 +56,7 @@ export const UserProfile = () => {
 
           <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
             {/* premiumUser is to be a boolean */}
-            {userData && userData.premiumUser ? (
+            {userData && userData?.premiumUser ? (
               <button className="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
                 Create Events
               </button>
@@ -67,24 +67,26 @@ export const UserProfile = () => {
                 </button>
               </Link>
             )}
-            <UserEditProfileButton />
+            <UserEditProfileButton formData={userData} />
           </div>
         </div>
 
         <div className="font-normal font-poppins mt-20 text-center border-b pb-12">
-          <h1 className="text-4xl font-medium text-gray-700">
-            {userData.firstName} {userData.lastName}
-          </h1>
+            <h1 className="text-4xl font-medium text-gray-700">
+              {userData.firstName} {userData.lastName} <span className="font-light text-gray-500">{userData.country}</span>
+            </h1>
+          <p className="font-light text-gray-600 mt-3">
+            {`${userData.address || ""}`}
+          </p>
           <p className="font-light text-gray-600 mt-3">
             {`${userData.email}`}
           </p>
           <p className="font-light text-gray-600 mt-3">{`#${userData.userName}`}</p>
         </div>
-        
         <div className="font-normal font-poppins mt-12 flex flex-col justify-center">
-          {userData && userData.about ? (
-            <p className="text-gray-600 text-center font-light lg:px-16">
-              {userData.about}
+          {userData ? (
+            <p className="text-gray-600 text-center font-light lg:px-16 text-left">
+              {userData?.about}
             </p>
           ) : (
             <p className="text-gray-600 text-center font-light lg:px-16">
