70163dc0ad3107ce61b28b86b65d4ebc7b8cc84f
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-09-02 03:34:44 +0300

    Update UserProfileForm.jsx

--- src/components/UserProfileForm/UserProfileForm.jsx
+++ src/components/UserProfileForm/UserProfileForm.jsx
@@ -9,7 +9,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 
 
-const UserProfileForm = ({ onClose, formData, onUpdate }) => {
+const UserProfileForm = ({ onClose, formData }) => {
     const { userData } = useContext(AuthContext);
 
     const [userProfileData, setUserProfileData] = useState({});
@@ -48,8 +48,8 @@ const UserProfileForm = ({ onClose, formData, onUpdate }) => {
     useEffect(() => {
         setUserProfileData(userData);
         setUserAbout(userData.about || "");
-        setIsActive(userData.isActive)
-        setAddressLocation(userData.address)
+        setIsActive(userData.isActive )
+        setAddressLocation(userData.address || "")
     }, [userData]);
 
     console.log(userProfileData);
@@ -88,8 +88,6 @@ const UserProfileForm = ({ onClose, formData, onUpdate }) => {
         await updateUserProfile(userData.uid, updatedProfile);
         setIsActive(true)
 
-        onUpdate(userProfileData);
-
         onClose();
 
     };
@@ -101,7 +99,6 @@ const UserProfileForm = ({ onClose, formData, onUpdate }) => {
     return (
         <section>
             <div className="overlay w-full bg-primary bg-opacity-70 h-screen flex justify-center items-center fixed left-0 top-0 px-6 py-8 mx-auto md:h-screen lg:py-0">
-                {/* Your logo and other header content */}
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <div className="grid justify-items-center">
@@ -124,12 +121,10 @@ const UserProfileForm = ({ onClose, formData, onUpdate }) => {
                                 <input
                                     type="text"
                                     {...register("firstName")}
-                                    value={userProfileData.firstName}
+                                    value={userProfileData.firstName || ''}
                                     onChange={(e) => { handleInputChange("firstName", e.target.value) }}
-                                    placeholder={`${userProfileData?.firstName}`}
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-
-                                />
+                               
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                                 <div className="invalid-feedback">{errors.firstName?.message}</div>
                             </div>
                             <div>
@@ -137,31 +132,27 @@ const UserProfileForm = ({ onClose, formData, onUpdate }) => {
                                     type="text"
                                     {...register("lastName")}
                                     onChange={(e) => { handleInputChange("lastName", e.target.value) }}
-                                    placeholder={`${userProfileData?.lastName}`}
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-
-                                />
-                                <div className="invalid-feedback">{errors.lastName?.message}</div>
+                                    value={userProfileData.lastName || ''}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                               <div className="invalid-feedback">{errors.lastName?.message}</div>
                             </div>
                             <div>
                                 <input
                                     type="text"
                                     {...register("email")}
                                     onChange={(e) => { handleInputChange("email", e.target.value) }}
-                                    placeholder={`${userProfileData?.email}`}
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                />
-                                <div className="invalid-feedback">{errors.email?.message}</div>
+                                    value={userProfileData.email || ''}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                               <div className="invalid-feedback">{errors.email?.message}</div>
                             </div>
                             <div>
                                 <input
                                     type="text"
                                     {...register("phone")}
                                     onChange={(e) => handleInputChange("phone", e.target.value)}
+                                    value={userProfileData.phone || ''}
                                     placeholder={`${userProfileData?.phone}`}
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-
-                                />
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                                 <div className="invalid-feedback">{errors.phone?.message}</div>
                             </div>
                             <div>
@@ -170,10 +161,8 @@ const UserProfileForm = ({ onClose, formData, onUpdate }) => {
                                     value={addressLocation || ""}
                                     onChange={(e) => setAddressLocation( e.target.value)}
                                     placeholder={`Street №, City`}
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-
-                                />
-                                <div className="invalid-feedback">{errors.phone?.message}</div>
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                                <div className="invalid-feedback">{errors.addressLocation?.message}</div>
                             </div>
                             <div>
                                 <textarea
@@ -181,33 +170,25 @@ const UserProfileForm = ({ onClose, formData, onUpdate }) => {
                                     value={userAbout || ""}
                                     onChange={(e) => setUserAbout(e.target.value)}
                                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required
-                                />
+                                    required/>
                             </div>
                             <input
                                 type="file"
                                 accept="image/*"
                                 onChange={handleAvatarChange}
-                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            />
+                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                             <label className="relative inline-flex items-center mr-5 cursor-pointer">
                                 <input
                                     type="checkbox"
                                     value=""
                                     className="sr-only peer"
-                                    checked={!isActive} // Connect the checkbox state to the isActive state
-                                    onChange={handleIsActive} // Handle the change event to toggle the state
-                                />
+                                    checked={!isActive}
+                                    onChange={handleIsActive} />
                                 <div className={`w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 ${isActive ? 'peer-checked:bg-red-600' : 'dark:bg-gray-700'} ${isActive ? 'peer-focus:ring-red-300' : 'dark:peer-focus:ring-red-800'} dark:border-gray-600 dark:peer-focus:ring-red-800 dark:peer-checked:after:translate-x-full dark:peer-checked:bg-red-800 after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:after:border-white`}></div>
                                 <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Incognito</span>
                             </label>
                             <div>
-                            <button
-                                type="submit"
-                                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                            >
-                                Update
-                            </button>
+                            <button type="submit"  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Update</button>
                             </div>
                         </form>
                     </div>
