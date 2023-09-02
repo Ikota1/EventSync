9c805acf9ebb9f2cb0ecd054bc4415f0f5c18ece
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-02 23:15:02 +0300

    Update UserEditProfileButton.jsx; refactor code

--- src/components/UserEditProfileButton/UserEditProfileButton.jsx
+++ src/components/UserEditProfileButton/UserEditProfileButton.jsx
@@ -1,24 +1,23 @@
 import UserProfileForm from "../UserProfileForm/UserProfileForm";
 import { useState } from "react";
 
-const UserEditProfileButton = () => {
+const UserEditProfileButton = ( {formData} ) => {
   const [showUpdateFrom, setShowUpdateForm] = useState(false);
-
   const handleShowForm = () => {
     setShowUpdateForm(!showUpdateFrom);
   };
-
-  const handleCloseForm = () => {
+  
+  const handleCloseForm = (updatedData) => {
     setShowUpdateForm(false);
   };
-
+  
   return (
     <div>
       <button className="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
        onClick={handleShowForm}>Edit Profile</button>
       {showUpdateFrom && (
         <div className="overlay">
-          <UserProfileForm onClose={handleCloseForm} />
+          <UserProfileForm formData={formData} onClose={handleCloseForm} />
         </div>
       )}
     </div>
