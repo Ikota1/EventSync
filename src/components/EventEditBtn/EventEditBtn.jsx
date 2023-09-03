7d08022859e80857da0c4a94cc28b79a678ce0da
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-09-03 07:20:52 +0300

    Add EventEditBtn.jsx

---
+++ src/components/EventEditBtn/EventEditBtn.jsx
@@ -0,0 +1,11 @@
+
+const EventEditBtn = ({ eventId }) => {
+
+// TBD 
+
+  return (
+    <button  className="bg-blue-700 text-white px-2 py-1 rounded mb-4">Edit Event</button>
+  )
+}
+
+export default EventEditBtn
