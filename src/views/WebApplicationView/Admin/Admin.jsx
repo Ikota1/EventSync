f4c1a0b8b26ec8f266ec636efcf102eef5d0c9b5
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-09-03 07:22:56 +0300

    Update Admin.jsx
    UI enhancement & refactoring

--- src/views/WebApplicationView/Admin/Admin.jsx
+++ src/views/WebApplicationView/Admin/Admin.jsx
@@ -1,33 +1,26 @@
 import styles from "../../../style"
-import { getAllUsers, blockUser, unblockUser, promoteUserToPremium } from '../../../services/user.services'
+import { getAllUsers } from '../../../services/user.services'
 import { useEffect, useState } from "react"
 import { getAllEvents } from "../../../services/events.service"
+import { NavLink, Outlet, useNavigate } from "react-router-dom"
+
 
 export const Admin = () => {
-    const [searchItem, setSearchItem] = useState('')
-    const [selectedSearchType, setSelectedSearchType] = useState('email');
-    const [filteredUsers, setFilteredUsers] = useState([])
     const [users, setUsers] = useState([])
     const [events, setEvents] = useState([])
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(true)
 
+    const navigate = useNavigate();
+    
 
     useEffect(() => {
         const getUsers = async () => {
             try {
                 const fetchedUsers = await getAllUsers();
-
                 setUsers(fetchedUsers);
-                setFilteredUsers(fetchedUsers);
 
             } catch (error) {
                 console.error('Error:', error);
-                setError(error);
-            } finally {
-
-                setLoading(false)
-            }
+            } 
         };
 
         getUsers();
@@ -36,7 +29,6 @@ export const Admin = () => {
 
     useEffect(() => {
         const getEvents = async () => {
-
             try {
                 const fetchedEvents = await getAllEvents()
                 setEvents(fetchedEvents)
@@ -49,115 +41,53 @@ export const Admin = () => {
         getEvents();
     }, [])
 
-    const handleSearchTypeChange = (e) => {
-        setSelectedSearchType(e.target.value);
-    };
 
-    const handleInputChange = (e) => {
-        const searchTerm = e.target.value.toLowerCase();
+return (
+    <>
+      <div className='text-white'>Admin</div>
+      <div className={`flex justify-center ${styles.boxWidth}`}>
+        <div className={`flex justify-between space-x-6 text-white ${styles.flexCenter}`}>
 
-        setSearchItem(searchTerm);
+          {location.pathname !== '/application/admin/controlusers' &&
+          location.pathname !== '/application/admin/controlevents' && (
+            <>
+              <div className="flex flex-col items-center border border-gray-300 rounded-lg p-6">
+                <div className="border-b border-purple-700 mb-2 pb-2 bg-blend-color">Total registered users</div>
+                <div className="text-3xl font-semibold">{users.length}</div>
+              </div>
+              <div className="flex flex-col items-center border border-gray-300 rounded-lg p-6">
+                <div className="border-b border-purple-700 mb-2 pb-2">Total Events created</div>
+                <div className="text-3xl font-semibold">{events.length}</div>
+              </div>
+              <div className="flex flex-col items-center border border-gray-300 rounded-lg p-6">
+                <div className="border-b border-purple-700 mb-2 pb-2">Tickets sold to date</div>
+                <div className="text-3xl font-semibold">0</div>
+              </div>
+            </>
+          )}
 
-        const searchWords = searchTerm.split(" ");
-
-        let filteredItems = [];
-
-        if (selectedSearchType === 'fullname') {
-            filteredItems = users.filter((user) => {
-                const fullName = `${user.firstName.toLowerCase()} ${user.lastName.toLowerCase()}`;
-                return searchWords.every(word => fullName.includes(word));
-            });
-        } else if (selectedSearchType === 'email') {
-            filteredItems = users.filter((user) => user.email.toLowerCase().includes(searchTerm));
-        } else if (selectedSearchType === 'username') {
-            filteredItems = users.filter((user) => user.userName.toLowerCase().includes(searchTerm));
-        }
-
-        setFilteredUsers(filteredItems);
-    };
-
-    const handleBlockUser = async (uid) => {
-        try {
-            await blockUser(uid);
-        } catch (error) {
-            console.error('Error:', error)
-        }
-
-    }
-    const handleUnblockUser = async (uid) => {
-        try {
-            await unblockUser(uid);
-        } catch (error) {
-            console.error('Error:', error)
-        }
-
-    }
-    const handlePremiumUser = async (uid) => {
-        try {
-            await promoteUserToPremium(uid);
-        } catch (error) {
-            console.error('Error:', error)
-        }
-    }
-    
-    return (
-        <>
-          <div className='text-white'>Admin</div>
-            <div className={`flex justify-center ${styles.boxWidth}`}>
-                <div className={`flex justify-between space-x-6 text-white ${styles.flexCenter}`}>
-                    <div className="flex flex-col items-center border border-gray-300 rounded-lg p-6">
-                        <div className="border-b border-gray-300 mb-2 pb-2 bg-blend-color">Total registred users</div>
-                        <div className="text-3xl font-semibold">{users.length}</div>
-                    </div>
-                    <div className="flex flex-col items-center border border-gray-300 rounded-lg p-6">
-                        <div className="border-b border-gray-300 mb-2 pb-2">Total Events created</div>
-                        <div className="text-3xl font-semibold">{events.length}</div>
-                    </div>
-                    <div className="flex flex-col items-center border border-gray-300 rounded-lg p-6">
-                        <div className="border-b border-gray-300 mb-2 pb-2">Tickets sold to date</div>
-                        <div className="text-3xl font-semibold">0</div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="flex justify-center mt-8">
-                <input
-                    type="text"
-                    value={searchItem}
-                    onChange={handleInputChange}
-                    className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
-                    placeholder="Type to search.." />
-                <select value={selectedSearchType} onChange={handleSearchTypeChange} className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500">
-                    <option value="fullname">FirstName & LastName</option>
-                    <option value="email">Email</option>
-                    <option value="username">Username</option>
-                </select>
-
-                {loading && <p>Loading...</p>}
-                {error && <p>There was an error loading the users</p>}
-
-            </div>
-            <div className="flex justify-start">
-                {!loading && !error && filteredUsers.length === 0
-                    ? <p className="text-white">No users found</p>
-                    : <ul>
-                        {filteredUsers.map(user => (
-                            <li key={user.uid} className="mt-2 text-white">
-                                <div>
-                                    {`Firstname: ${user.firstName} Lastname: ${user.lastName} Username: ${user.userName} Email: ${user.email}`}
-                                    <button onClick={() => handleBlockUser(user.uid)} className="bg-blue-500 text-white px-2 py-1 rounded"> Block </button>
-                                    <button onClick={() => handleUnblockUser(user.uid)} className="bg-red-500 text-white px-2 py-1 rounded "> Unblock </button>
-                                    <button onClick={() => handlePremiumUser(user.uid)} className="bg-green-500 text-white px-2 py-1 rounded "> Premium </button>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                }
-            </div>
-        </>
-    )
+          {(location.pathname === '/application/admin/controlusers' ||
+            location.pathname === '/application/admin/controlevents') && (
+            <button onClick={() => navigate('/application/admin')} className="bg-blue-700 text-white px-2 py-1 rounded">Back to Admin Charts</button>
+          )}
+        </div>
+      </div>
+      <div className="h-[100%]">
+        <div className='flex justify-center pb-8'>
+        {location.pathname !== '/application/admin/controlusers' &&
+          location.pathname !== '/application/admin/controlevents' && (
+          <nav className="mt-6">
+            <ul className="flex space-x-6">
+              <li><NavLink to="controlusers" className="bg-purple- text-white px-2 py-1 rounded"> Control Users</NavLink></li>
+              <li><NavLink to="controlevents" className="bg-purple-500 text-white px-2 py-1 rounded"> Control Events</NavLink></li>
+            </ul>
+          </nav>
+           )}
+        </div>
+        <Outlet />
+      </div>
+    </>
+  )
 }
 
-
-export default Admin
-
+export default Admin;
