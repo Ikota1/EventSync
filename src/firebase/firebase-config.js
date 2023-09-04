59fa48acf16d8c486c4a8c12a65d6e1de8130280
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-04 23:19:41 +0300

    Update firebase-config.js; Add new config

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,15 +3,29 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 import { getStorage } from "firebase/storage";
 
+//DEPRECATED FIREBASE CONFIG - EXCEEDED RL DB USAGE
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA5oiMRWYDLoAWEJNQjblPnjV64TcjwGE8",
+//   authDomain: "event-calendar-96f71.firebaseapp.com",
+//   databaseURL: "https://event-calendar-96f71-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "event-calendar-96f71",
+//   storageBucket: "event-calendar-96f71.appspot.com",
+//   messagingSenderId: "192765244619",
+//   appId: "1:192765244619:web:bd10baa466b948a389574e"
+// };
+
+
+//NEW FIRABASE CONFIG - 23/09/04
 const firebaseConfig = {
-  apiKey: "AIzaSyA5oiMRWYDLoAWEJNQjblPnjV64TcjwGE8",
-  authDomain: "event-calendar-96f71.firebaseapp.com",
-  databaseURL: "https://event-calendar-96f71-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "event-calendar-96f71",
-  storageBucket: "event-calendar-96f71.appspot.com",
-  messagingSenderId: "192765244619",
-  appId: "1:192765244619:web:bd10baa466b948a389574e"
+  apiKey: "AIzaSyCdUvQX7adFTAGQTKylxfKOTCvLLFQfxsg",
+  authDomain: "eventsync-1.firebaseapp.com",
+  databaseURL: "https://eventsync-1-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "eventsync-1",
+  storageBucket: "eventsync-1.appspot.com",
+  messagingSenderId: "279476340668",
+  appId: "1:279476340668:web:90abfb95a53c13321f4db9"
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
