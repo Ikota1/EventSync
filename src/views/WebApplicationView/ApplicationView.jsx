6124646781e7c54a467a5a8f82153785c5ef4795
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-06 14:50:37 +0300

    Update ApplicationView; Add initial photo

--- src/views/WebApplicationView/ApplicationView.jsx
+++ src/views/WebApplicationView/ApplicationView.jsx
@@ -14,15 +14,25 @@ const ApplicationView = () => {
     nav('/application/settings')
   }
 
+  const getInitials = (firstName, lastName) => {
+    return `${firstName?.charAt(0) || ""}${lastName?.charAt(0) || ""}`.toUpperCase();
+  };
+
   return (
     <div className="flex">
       <Sidebar />
       <div className='flex-1 h-screen overflow-hidden'>
         <div className='flex justify-end bg-gray-700 px-7 py-2 items-center gap-4'>
-        <div onClick={handlePhotoClick} className="h-12 w-12 flex-shrink-0 ">
-      <img className="h-full w-full rounded-full" src={userData?.photo} alt="Avatar" />
-      </div>
-          <NotificationCenter/>
+          <div onClick={handlePhotoClick} className="h-12 w-12 flex-shrink-0 ">
+            {userData && userData.photo ? (
+              <img className="h-full w-full rounded-full" src={userData.photo} alt="Avatar" />
+            ) : (
+              <span className="h-full w-full rounded-full flex items-center justify-center bg-indigo-100">
+                {getInitials(userData?.firstName, userData?.lastName)}
+              </span>
+            )}
+          </div>
+          <NotificationCenter />
         </div>
         <div className="flex-1 p-7" >
           <Outlet />
