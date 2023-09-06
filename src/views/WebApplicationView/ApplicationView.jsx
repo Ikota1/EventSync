a4ac4cc5a6db4513762dfb7dee7f177464c19e7a
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-09-06 06:29:36 +0300

    Add NavBar to appView
    Holds userPhoto & notificaiton bell

--- src/views/WebApplicationView/ApplicationView.jsx
+++ src/views/WebApplicationView/ApplicationView.jsx
@@ -1,13 +1,32 @@
 import Sidebar from '../../components/Sidebar/Sidebar';
+import NotificationCenter from '../../components/NotificationCenter/NotificationCenter';
+import { AuthContext } from '../../context/UserContext';
+import { useNavigate } from 'react-router-dom';
 
 import { Outlet } from "react-router-dom";
+import { useContext } from 'react';
 
 const ApplicationView = () => {
+  const { userData } = useContext(AuthContext);
+  const nav = useNavigate();
+
+  const handlePhotoClick = () => {
+    nav('/application/settings')
+  }
+
   return (
     <div className="flex">
       <Sidebar />
-      <div className="h-screen flex-1 p-7">
-        <Outlet />
+      <div className='flex-1 h-screen overflow-hidden'>
+        <div className='flex justify-end bg-gray-700 px-7 py-2 items-center gap-4'>
+        <div onClick={handlePhotoClick} className="h-12 w-12 flex-shrink-0 ">
+      <img className="h-full w-full rounded-full" src={userData?.photo} alt="Avatar" />
+      </div>
+          <NotificationCenter/>
+        </div>
+        <div className="flex-1 p-7" >
+          <Outlet />
+        </div>
       </div>
     </div>
   )
