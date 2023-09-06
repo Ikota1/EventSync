a2459741af6ab00491db9b684cc0a4b9b1a38fc4
Daniil <dnazarov870@gmail.com>
2023-09-06 20:06:33 +0300

    Add Animation to Landing Page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import styles from '../../style';
 import GetStarted from '../GetStarted/GetStarted';
+import Lottie from 'lottie-react';
+import animationData from '../../assets/Ly6OsxGIXU.json'
 
 const Hero = () => (
   <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
@@ -24,6 +26,7 @@ const Hero = () => (
 
     <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
 
+      <Lottie animationData={animationData} />
       <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
       <div className='absolute z-[1] w-[80%] h-[80%] bottom-40 white__gradient' />
       <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
