91e290d96c10763b9fc6c973404cfe21d2209201
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-09-07 02:10:19 +0300

    Update Friends.jsx

--- src/views/WebApplicationView/Friends/Friends.jsx
+++ src/views/WebApplicationView/Friends/Friends.jsx
@@ -4,6 +4,7 @@ import { useContext, useEffect, useState } from "react"
 import { getUserByHandle } from "../../../services/user.services";
 import FriendsLinks from "./FriendsLinks";
 import { deleteFriend } from "../../../services/social.service";
+import toast from 'react-hot-toast'
 
 
 const Friends = () => {
@@ -33,9 +34,14 @@ const Friends = () => {
     fetchData();
   }, [userData]);
 
-    const handleRemoveFriendBtnClick = async (userToDeleteID) => {
-
-    await deleteFriend(userData.uid, userToDeleteID)
+  const handleRemoveFriendBtnClick = async (userToDeleteID, deletedUserName) => {
+    try {
+      await deleteFriend(userData.uid, userToDeleteID)
+      toast.success(`${deletedUserName} has been deleted from your Friendlist.`)
+    } catch (error) {
+      console.error(error)
+      toast.error(`Unable to delete ${deletedUserName} from your Friendlist. `)
+    }
 
   }
 
@@ -45,23 +51,23 @@ const Friends = () => {
       <div className="grid grid-cols-4 gap-2 py-8">
         {userFriends.length === 0 ? (
           <div className="flex items-center justify-center col-span-4"> {/* Use col-span-4 to span all columns */}
-          <p className="text-center text-gray-500 dark:text-gray-400">Your friendlist is empty.</p>
-        </div>
+            <p className="text-center text-gray-500 dark:text-gray-400">Your friendlist is empty.</p>
+          </div>
         ) : (
           userFriends.map((user) => (
-            <div key={user.uid} className="w-full max-w-sm pt-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div key={user?.uid} className="w-full max-w-sm pt-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <div className="flex flex-col items-center pb-10">
-                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={user.photo} alt={`${user.firstName}'s profile`} />
-                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{user.firstName} {user.lastName}</h5>
-                <span className="text-sm text-gray-500 dark:text-gray-400">{user.userName}</span>
-                <p className="text-sm text-gray-500 dark:text-gray-400">{user.country}</p>
-                <p className="text-sm text-gray-500 dark:text-gray-400">{user.email}</p>
-                <p className="text-sm text-gray-500 dark:text-gray-400">{user.phone}</p>
+                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={user?.photo} alt={`${user?.firstName}'s profile`} />
+                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{user?.firstName} {user?.lastName}</h5>
+                <span className="text-sm text-gray-500 dark:text-gray-400">{user?.userName}</span>
+                <p className="text-sm text-gray-500 dark:text-gray-400">{user?.country}</p>
+                <p className="text-sm text-gray-500 dark:text-gray-400">{user?.email}</p>
+                <p className="text-sm text-gray-500 dark:text-gray-400">{user?.phone}</p>
                 <div className="flex mt-4 space-x-3 md:mt-6">
                 </div>
               </div>
               <div className="flex justify-end px-4 pt-4">
-                <button onClick={() => handleRemoveFriendBtnClick(user.uid)} className="bg-blue-500 text-white px-2 py-1 rounded mb-4">Remove Friend</button>
+                <button onClick={() => handleRemoveFriendBtnClick(user.uid, user.userName)} className="bg-blue-500 text-white px-2 py-1 rounded mb-4">Remove Friend</button>
               </div>
             </div>
           ))
@@ -70,6 +76,6 @@ const Friends = () => {
       <Outlet />
     </>
   );
-  
-          }
+
+}
 export default Friends
