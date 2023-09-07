1d3f55671e2fced5460948cb9e44692ce70cd49c
Daniil <dnazarov870@gmail.com>
2023-09-07 04:24:45 +0300

    Update LandingPage UI

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => (
       </p>
     </div>
 
-    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative flex-col`}>
 
       <Lottie animationData={animationData} />
       <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
