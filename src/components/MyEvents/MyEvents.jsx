16d463bb0a1b430f9c35c160a30dba34e1ae3e0d
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-09-08 08:11:57 +0300

    Update MyEvents - Refactor & Enhancement

--- src/components/MyEvents/MyEvents.jsx
+++ src/components/MyEvents/MyEvents.jsx
@@ -4,35 +4,50 @@ import { auth } from '../../firebase/firebase-config';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import format from 'date-fns/format';
 import EventDeleteBtn from '../EventDeleteBtn/EventDeleteBtn';
+import EventLinks from '../../views/WebApplicationView/Events.jsx/EventLinks';
 
 const MyEvents = ({ onBackToPublicClick }) => {
   const [user] = useAuthState(auth);
   const [myEventsData, setMyEventsData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+
   const eventsPerPage = 4;
 
   useEffect(() => {
     const fetchUserEvents = async () => {
-      if (user) {
-        const userEventsSnapshot = await getEventsByCurrentUser(user.uid);
-        const userEventsArray = userEventsSnapshot.val();
+      setLoading(true)
 
-        if (userEventsArray) {
-          const eventDataPromises = userEventsArray.map(async (eventID) => {
-            const eventSnapshot = await getEventByHandle(eventID);
-            return eventSnapshot.exists() ? eventSnapshot.val() : null;
-          });
-          const eventDataArray = await Promise.all(eventDataPromises);
-          setMyEventsData(eventDataArray.filter(eventData => eventData !== null));
+      try {
+        if (user) {
+          const userEventsSnapshot = await getEventsByCurrentUser(user.uid);
+          const userEventsArray = userEventsSnapshot.val();
+  
+          if (userEventsArray) {
+            const eventDataPromises = userEventsArray.map(async (eventID) => { 
+              const eventSnapshot = await getEventByHandle(eventID);
+  
+              return eventSnapshot.exists() ? eventSnapshot.val() : null;
+            });
+            const eventDataArray = await Promise.all(eventDataPromises);
+  
+            setMyEventsData(eventDataArray.filter(eventData => eventData !== null));
+          }
         }
+        
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setLoading(false);
       }
+    
     };
 
 
     fetchUserEvents();
   }, [user]);
 
-  console.log(myEventsData)
+
   const handleNextPage = () => {
     const totalPages = Math.ceil(myEventsData.length / eventsPerPage);
     if (currentPage < totalPages) {
@@ -57,36 +72,42 @@ const MyEvents = ({ onBackToPublicClick }) => {
 
   return (
     <>
-      <div>
-        <div className="grid grid-cols-4 gap-4">
-          {paginatedEvents.map(event => (
-            <div key={event.id} className="bg-gray-900 text-blue-300 rounded-lg shadow-md p-4 hover:bg-gray-800 hover:text-blue-400 transition-transform duration-300 transform scale-100 hover:scale-105">
-             {console.log('ID', event.id)}
-              <img src={event.photo} alt={event.title} className="w-full h-60 object-cover rounded-lg mb-4" />
-              <h2 className="text-lg font-semibold">{event.title}</h2>
-              <p className='pt-6 pb-6'>{event.description}</p>
-              <p className='pb-4'>Location: {event.location}</p>
-              <p>{format(new Date(event.startDate), "do MMM")} | {event.startHour}h - {event.endHour}h</p>
-              <p>Type: {event.isOnline ? 'Online' : 'Live'}</p>
-              <EventDeleteBtn eventId={event.id} onDelete={() => handleEventDelete(event.id)} />
-            </div>
-          ))}
+      <EventLinks />
+      {loading ? (
+        <div className="flex justify-center items-center h-full">
+          <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-blue-400"></div>
         </div>
-        {/* Pagination controls */}
-        <div className={`fixed bottom-0 right-0 py-2 px-6 shadow`}>
-          <div className="pagination text-blue-500">
-            <button onClick={handlePreviousPage} className="mr-2 h-12 w-12 rounded-full bg-blue-700 border-blue-600 text-sm text-white transition duration-150 hover:bg-blue-500" disabled={currentPage === 1}>Prev</button>
-            <button onClick={handleNextPage} className="mr-2 h-12 w-12 rounded-full bg-blue-700 border-blue-600 text-sm  text-white transition duration-150 hover:bg-blue-500">Next</button>
+      ) : myEventsData.length === 0 ? (
+        <div className="flex justify-center items-center h-full">
+          <p className="text-blue-300 text-4xl">No Events Found</p>
+        </div>
+      ) : (
+        <div>
+          <div className="grid grid-cols-4 gap-4">
+            {paginatedEvents.map((event) => (
+              <div key={event.id} className="bg-gray-900 text-blue-300 rounded-lg shadow-md p-4 hover:bg-gray-800 hover:text-blue-400 transition-transform duration-300 transform scale-100 hover:scale-105" >
+                <img src={event.photo} alt={event.title} className="w-full h-60 object-cover rounded-lg mb-4"/>
+                <h2 className="text-lg font-semibold">{event.title}</h2>
+                <p className="pt-6 pb-6">{event.description}</p>
+                <p className="pb-4">Location: {event.location}</p>
+                <p>{format(new Date(event.startDate), 'do MMM')} | {event.startHour}h - {event.endHour}h</p>
+                <p>Type: {event.isOnline ? 'Online' : 'Live'}</p>
+                <EventDeleteBtn eventId={event.id} onDelete={() => handleEventDelete(event.id)} />
+              </div>
+            ))}
+          </div>
+          {/* Pagination controls */}
+          <div className={`fixed bottom-0 right-0 py-2 px-6 shadow`}>
+            <div className="pagination text-blue-500">
+              <button onClick={handlePreviousPage} className="mr-2 h-12 w-12 rounded-full bg-blue-700 border-blue-600 text-sm text-white transition duration-150 hover:bg-blue-500"disabled={currentPage === 1}>Prev </button>
+              <button onClick={handleNextPage} className="mr-2 h-12 w-12 rounded-full bg-blue-700 border-blue-600 text-sm  text-white transition duration-150 hover:bg-blue-500" > Next</button>
+            </div>
           </div>
         </div>
-        <div>
-          <button className="relative  bg-blue-700 text-white px-2 py-1 rounded" onClick={onBackToPublicClick}>Browse Public Events</button>
-        </div>
-      </div>
+      )}
     </>
   );
 };
 
 export default MyEvents;
-
 
