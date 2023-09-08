bcff888f4329de6c774329ca0507e65e77e89236
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-08 13:32:58 +0300

    Add EventLocation.jsx; draft visual of an address

---
+++ src/components/EventMap/EventLocation.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
+import { useLocation } from 'react-router-dom';
+
+const EventLocation = ({ userLocation }) => {
+    const [userDefLocation, setDefUserLocation] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    const getUserLocation = async () => {
+        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${userLocation}&key=AIzaSyArxnIuFJJezK4Zo99XnTJLy-wm1piTrBw`)
+        const location = await response.json()
+        setDefUserLocation(location.results[0].geometry.location)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        getUserLocation()
+    }, [])
+
+
+    if (loading) {
+        return null
+    }
+
+    return (
+        <div>
+            <GoogleMap
+                center={userDefLocation}
+                zoom={14}
+                mapContainerStyle={{ width: '400px', height: '400px' }}
+            >
+                <Marker position={userDefLocation} />
+            </GoogleMap>
+        </div>
+    );
+};
+
+export default EventLocation;
