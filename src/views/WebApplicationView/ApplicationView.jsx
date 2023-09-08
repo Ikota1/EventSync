ba17acc5b21eaba6cafb6eeff85398fd10db117f
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-09 01:18:48 +0300

    Update ApplicationView; Add DND toggle

--- src/views/WebApplicationView/ApplicationView.jsx
+++ src/views/WebApplicationView/ApplicationView.jsx
@@ -7,9 +7,26 @@ import { Outlet } from "react-router-dom";
 import { useContext } from 'react';
 import { getInitials } from '../../constants/helpersFns/getInitials';
 
+import { useState,useEffect } from 'react';
+import { updateUserProfile } from '../../services/user.services';
+
+
 const ApplicationView = () => {
   const { userData } = useContext(AuthContext);
   const nav = useNavigate();
+
+  const [doNotDisturb, setDoNotDisturb] = useState(true);
+
+  useEffect(() => {
+    setDoNotDisturb(userData?.doNotDisturb)
+  }, [userData]);
+
+  const handleDoNotDisturb = async () => {
+    let updatedProfile = { ...userData, doNotDisturb: !doNotDisturb };
+
+    await updateUserProfile(userData.uid, updatedProfile);
+    setDoNotDisturb(!doNotDisturb)
+  }
 
   const handlePhotoClick = () => {
     nav('/application/settings')
@@ -30,7 +47,25 @@ const ApplicationView = () => {
             )}
           </div>
           <NotificationCenter />
-        </div>
+          <label className="relative flex mb-5 cursor-pointer items-center justify-center mt-5">
+            <input
+              type="checkbox"
+              value=""
+              className="sr-only peer"
+              checked={doNotDisturb || ''}
+              onChange={handleDoNotDisturb}
+            />
+            <div className={`
+              w-9 h-5 bg-gray-200 rounded-full peer
+              ${doNotDisturb === true ? 'peer-checked:bg-purple-500' : 'dark:bg-gray-300'}
+              dark:border-gray-600  dark:peer-checked:after:translate-x-full
+              dark:peer-checked:bg-purple-500 after:content-[''] after:absolute after:top-0.5
+              after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full
+              after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:after:border-white
+            `}></div>
+            <span className={`${doNotDisturb === true ? `ml-3 text-sm font-normal font-poppins text-purple-500` : `ml-3 text-sm font-normal font-poppins text-gray-900 dark:text-gray-300`} `}>DND</span>
+          </label>
+        </div>        
         <div className="flex-1 p-7" >
           <Outlet />
         </div>
