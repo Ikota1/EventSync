c4528027d6db64b8add641b51ab27c0a644be4a9
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-08 13:31:05 +0300

    Update App.jsx; Add google API key

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import LandingPage from "./views/LandingPageView/LandingPage/LandingPage";
 import { AuthContextProvider } from "./context/UserContext";
 import { AppContextProvider } from "./context/AppContext";
 import { Toaster } from 'react-hot-toast'
+import {LoadScript } from '@react-google-maps/api';
 
 const App = () => (
-  <AuthContextProvider>
-    <AppContextProvider>
-      <BrowserRouter>
-        <div className="bg-primary w-full overflow-hidden">
-          {<LandingPage /> && <AppRouter />}
-        </div>
-        <Toaster position="top-right"/>
-      </BrowserRouter>
-    </AppContextProvider>
-  </AuthContextProvider>
+  <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_API_FILE}>
+    <AuthContextProvider>
+      <AppContextProvider>
+        <BrowserRouter>
+          <div className="bg-primary w-full overflow-hidden">
+            {<LandingPage /> && <AppRouter />}
+          </div>
+          <Toaster position="top-right" />
+        </BrowserRouter>
+      </AppContextProvider>
+    </AuthContextProvider>
+  </LoadScript>
 )
 
 
