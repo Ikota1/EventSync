83ea9d684851a261c58f07cebaef1b236386e44a
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-08 13:33:34 +0300

    Update EventsDetails; Add EventLocation component

--- src/views/WebApplicationView/EventsDetails/EventsDetails.jsx
+++ src/views/WebApplicationView/EventsDetails/EventsDetails.jsx
@@ -8,12 +8,15 @@ import Lottie from 'lottie-react';
 import instIcon from '../../../assets/animation_lm9cz07c.json'
 import linkedinIcon from '../../../assets/animation_lm9d7gvz.json'
 import clockIcon from '../../../assets/animation_lm9dbhqr.json'
+import EventLocation from '../../../components/EventMap/EventLocation';
 
 const EventsDetails = () => {
   const params = useParams()
   const [eventsDetailed, setEventsDetailed] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const fetchEventData = async () => {
+    setLoading(true);
     try {
       const eventId = ref(db, `events/${params.id}`)
       const snapshot = await get(eventId)
@@ -24,8 +27,10 @@ const EventsDetails = () => {
       } else {
         console.log('Thread not found.');
       }
+      setLoading(false)
     } catch (error) {
       console.error('Unable to fetch public events', error);
+      setLoading(false)
     }
   }
 
@@ -34,29 +39,30 @@ const EventsDetails = () => {
   }, [])
 
   return (
-    <div className='text-white flex justify-center flex-col gap-4 items-center'>
-      <h1 className='text-gradient uppercase text-4xl font-medium'>{eventsDetailed.title}</h1>
-      <span className='flex items-center gap-2'><Lottie className='w-[30px] bg-white rounded-full' animationData={clockIcon} /> {eventsDetailed.startDate} To {eventsDetailed.startDate}</span>
+      <div className='text-white flex justify-center flex-col gap-4 items-center'>
+        <h1 className='text-gradient uppercase text-4xl font-medium'>{eventsDetailed.title}</h1>
+        <span className='flex items-center gap-2'><Lottie className='w-[30px] bg-white rounded-full' animationData={clockIcon} /> {eventsDetailed.startDate} To {eventsDetailed.startDate}</span>
 
-      <TicketPurchaseBtn />
+        <TicketPurchaseBtn />
 
-      <img src={eventsDetailed.photo} alt="photo" className='w-[700px] h-[300px] rounded' />
+        <img src={eventsDetailed.photo} alt="photo" className='w-[700px] h-[300px] rounded' />
+        {!loading ? <EventLocation userLocation={eventsDetailed.location} /> : null}
 
-      <div className='flex flex-col gap-4 items-center'>
-        <p>{eventsDetailed.description}</p>
-        <hr className='w-[500px] opacity-20' />
-        <div className='flex justify-between w-[500px] items-center'>
-          <button className="bg-blue-500 text-white px-2 py-1 rounded mb-4" >
-            Add To Calendar
-          </button>
+        <div className='flex flex-col gap-4 items-start'>
+          <p>{eventsDetailed.description}</p>
+          <hr className='w-[500px] opacity-20' />
+          <div className='flex justify-between w-[500px] items-center'>
+            <button className="bg-blue-500 text-white px-2 py-1 rounded mb-4" >
+              Add To Calendar
+            </button>
 
-          <div className='flex gap-1'>
-            <Lottie className='w-[50px] cursor-pointer' animationData={instIcon} />
-            <Lottie className='w-[35px] cursor-pointer' animationData={linkedinIcon} />
+            <div className='flex'>
+              <Lottie className='w-[50px] cursor-pointer' animationData={instIcon} />
+              <Lottie className='w-[35px] cursor-pointer' animationData={linkedinIcon} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
   )
 }
 
