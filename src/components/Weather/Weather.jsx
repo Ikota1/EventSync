e44cc8be403433ea37928798f1a3db7bb3166b96
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-09 01:16:54 +0300

    Update Weather.jsx visual styling

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -2,16 +2,19 @@ import PropTypes from 'prop-types';
 
 export const LocationResults = ({ location: { location, current } }) => {
     return (
-        <div className="flex flex-col items-center p-8 rounded-md w-60 sm:px-12 dark:text-gray-100">
-            <div className="text-center">
-                <h2 className="text-xl font-semibold">{location?.name} / {location?.country}</h2>
-                <p className="text-sm dark:text-gray-400">{current?.last_updated}</p>
+        <div className="flex flex-row items-center p-3 justify-evenly rounded-md bg-gray-700 w-50 sm:px-5 dark:text-gray-100">
+            <div className='flex flex-col text-left'>
+                <div className="text-center">
+                    <h2 className="text-base font-normal font-poppins">{location?.name}</h2>
+                </div>
+                <div className="mb-2 text-sm font-normal font-poppins">{current?.temp_c}°
+                    <span className="mx-1 font-normal font-poppins">/</span>{current?.feelslike_c}°
+                </div>
             </div>
-            <img src={current?.condition.icon} alt={current?.condition.text} />
-            <div className="mb-2 text-3xl font-semibold">{current?.temp_c}°
-                <span className="mx-1 font-normal">/</span>{current?.feelslike_c}°
+            <div className='flex flex-col items-center'>
+                <img src={current?.condition.icon} alt={current?.condition.text} className='scale-55' />
+                {/* <p className="dark:text-gray-400 text-xs text-center font-poppins">{current?.condition.text}</p> */}
             </div>
-            <p className="dark:text-gray-400">{current?.condition.text}</p>
         </div>
     )
 }
