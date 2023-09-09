1fa3b948ac681cb3b3a790201bee34c07b858478
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-09-09 03:27:22 +0300

    Create AdminLinks.jsx

---
+++ src/views/WebApplicationView/Admin/AdminLinks.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from 'react-router-dom'
+
+const AdminLinks = () => {
+    return (
+        <div className="flex justify-center">
+            <nav className="mt-6">
+                <ul className="flex space-x-6">
+                    <li><NavLink to="../admin" className={({ isActive }) => (isActive ? 'text-orange-500' : "text-blue-500 hover:text-blue-300")}> Stats </NavLink></li>
+                    <li><NavLink to="../control-users" className={({ isActive }) => (isActive ? 'text-orange-500' : "text-blue-500 hover:text-blue-300")}> Control Users </NavLink></li>
+                    <li><NavLink to="../control-events" className={({ isActive }) => (isActive ? 'text-orange-500' : "text-blue-500 hover:text-blue-300")}> Control Events </NavLink></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default AdminLinks
