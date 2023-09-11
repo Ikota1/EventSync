dcd043ac4c836534199d8df12c4d909d3b6ddfbe
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-09-11 06:20:42 +0300

    Add LiveEvents Chart.

---
+++ src/components/BarChart/BarChartLiveEvents.jsx
@@ -0,0 +1,45 @@
+import dayjs from 'dayjs';
+import { useState } from 'react';
+import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from 'recharts';
+import PropTypes from 'prop-types';
+
+const BarChartLiveEvents = ({ events }) => {
+  const [eventsData] = useState(Array.from({ length: 7 }).map((event, index) => {
+    const date = dayjs()
+      .add(index - (7 - 1), 'day')
+      .format('DD/MM/YYYY');
+
+    const LiveEventsToday = events.filter((event) => {
+      const formattedDate = dayjs(event.createdOn).format('DD/MM/YYYY');
+      if(date === formattedDate && !event.isOnline)
+      return event;
+    });
+    console.log('live', LiveEventsToday.length)
+
+    return {
+      date,
+      events: LiveEventsToday.length,
+    };
+  }));
+
+  return (
+    <div className="text-center">
+    <h2 className="text-xl text-purple-700 mb-4">Live Events the Last 7 Days</h2>
+    <div className="flex justify-center">
+      <BarChart width={500} height={300} data={eventsData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="events" fill="#8884d8" />
+      </BarChart>
+    </div>
+  </div>
+  );
+};
+
+BarChartLiveEvents.propTypes = {
+  events: PropTypes.array.isRequired,
+};
+
+export default BarChartLiveEvents;
