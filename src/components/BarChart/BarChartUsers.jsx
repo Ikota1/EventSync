e8e99cbb5516960f81ecf15538133184f03cb6b0
angel.angelov711 <angel.krasimirov35@gmail.com>
2023-09-11 06:20:29 +0300

    Update Events & Users Charts

--- src/components/BarChart/BarChartUsers.jsx
+++ src/components/BarChart/BarChartUsers.jsx
@@ -27,13 +27,18 @@ const BarChartUsers = ({ users }) => {
 
 
   return (
-    <BarChart width={500} height={300} data={usersData}margin={{ top: 5, right: 30, left: 20, bottom: 5 }} >
-      <XAxis dataKey="date" />
-      <YAxis allowDecimals='false' />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="users" fill="#8884d8" />
-    </BarChart>
+    <div className="text-center">
+    <h2 className="text-xl text-purple-700 mb-4">Registred Users in the Last 7 Days</h2>
+    <div className="flex justify-center">
+      <BarChart width={500} height={300} data={usersData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="users" fill="#8884d8" />
+      </BarChart>
+    </div>
+  </div>
   );
 }
 
