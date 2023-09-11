24a79607b852299fbe63a435b986672e2c03e4d7
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-11 22:14:19 +0300

    Add about-us constants

---
+++ src/constants/about-us.js
@@ -0,0 +1,34 @@
+export const developers = [
+    {
+        id: 1,
+        name: 'Angel Angelov',
+        profession: "Web Developer",
+        description: "My goal is to not only write clean and efficient code but also to continuously learn and adapt to the ever-evolving world of web development, ensuring that the websites exceed user expectations.",
+        email: "a.krasimirov35@gmail.com",
+        service: "service service1",
+        gitlab:"https://gitlab.com/angel.angelov711",
+        linkedin:"",
+    },
+    {
+        id: 2,
+        name: 'Daniil Nazarov',
+        profession: "Web Developer",
+        description: "My goal is to not only write clean and efficient code but also to continuously learn and adapt to the ever-evolving world of web development, ensuring that the websites exceed user expectations.",
+        email: "dnazarov870@gmail.com",
+        service: "service service2",
+        gitlab:"https://gitlab.com/daniil.nazarov",
+        linkedin:"",
+    },
+    {
+        id: 3,
+        name: 'Kristiyan Todorov',
+        profession: "Web Developer",
+        description: "My goal is to not only write clean and efficient code but also to continuously learn and adapt to the ever-evolving world of web development, ensuring that the websites exceed user expectations.",
+        email: "yonkov.kristiyan@gmail.com",
+        service: "service service3",
+        gitlab:"https://gitlab.com/kristiyanT",
+        linkedin:"",
+    },
+]
+
+export default developers
