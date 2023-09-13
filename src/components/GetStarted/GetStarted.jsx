5a6abbe4d7b8d185c3ef8aa07bac7b17a90405f1
kristiyanT <kristiyan.todorov.a49@learn.telerikacademy.com>
2023-09-13 17:23:51 +0300

    Update GetStarted; Add bouncing effect

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -6,7 +6,7 @@ const GetStarted = () => {
   const navigate = useNavigate();
 
   return (
-    <div onClick={() => navigate('signup')} className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div onClick={() => navigate('signup')} className={`${styles.flexCenter} w-[140px] h-[140px] animate-bounce rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
           <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
